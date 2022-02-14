603be1d1e8c669de5836478143d81b03c019afa1
Nikita Smith <n.galkin.official@gmail.com>
2022-02-14 13:27:38 +0500

    docs: ✏️ storybook: +BarSmall component

---
+++ src/lib/containers/TaskItem/components/Bars/BarSmall.stories.tsx
@@ -0,0 +1,55 @@
+// *** NPM ***
+import React, { useState } from "react";
+
+// *** OTHER ***
+import BarSmall from "./BarSmall";
+import { BarTask } from "../../../../types/bar-task";
+
+export default {
+	title: "TaskItem/BarSmall",
+	component: BarSmall,
+};
+
+// Simple
+export const Simple = () => {
+	// *** USE STATE ***
+	const [task] = useState<BarTask>({
+		index: 1,
+		typeInternal: "task",
+		x1: 0,
+		x2: 200,
+		y: 0,
+		height: 50,
+		progressX: 0,
+		progressWidth: 100,
+		barCornerRadius: 5,
+		handleWidth: 10,
+		barChildren: [],
+		styles: {
+			backgroundColor: "black",
+			backgroundSelectedColor: "yellow",
+			progressColor: "green",
+			progressSelectedColor: "orange",
+		},
+		id: "1",
+		name: "Test",
+		start: new Date(2021, 10, 21),
+		end: new Date(2021, 10, 20),
+		progress: 50,
+		type: "task",
+		dependencies: [],
+		hideChildren: true,
+		isDisabled: false,
+		project: undefined,
+	});
+
+	return (
+		<BarSmall
+			task={task}
+			isSelected={false}
+			isDateChangeable={true}
+			isProgressChangeable={true}
+			onEventStart={(...args) => console.log(args)}
+		></BarSmall>
+	);
+};
--- src/lib/containers/TaskItem/components/Bars/BarSmall.tsx
+++ src/lib/containers/TaskItem/components/Bars/BarSmall.tsx
@@ -6,25 +6,41 @@ import { getProgressPoint } from "../../../../helpers/bar-helper";
 import BarDisplay from "./BarDisplay";
 import BarProgressHandle from "./BarProgressHandle";
 import { IProps as TaskItemProps } from "../../TaskItem";
+import { OptionalKeys } from "../../../../types/custom";
 
 // *** STYLES ***
 import styles from "./Bar.module.css";
 
 // *** TYPES ***
-type IProps = TaskItemProps;
+type IProps = {
+	rootStyle?: React.CSSProperties;
+} & Pick<
+	TaskItemProps,
+	| "task"
+	| "isSelected"
+	| "isDateChangeable"
+	| "isProgressChangeable"
+	| "onEventStart"
+>;
+type TOptionalPropsKeys = Exclude<OptionalKeys<IProps>, undefined>;
+type TOptionalProps = Required<Pick<IProps, TOptionalPropsKeys>>;
 
-const BarSmall = (props: IProps) => {
+const defaultProps: TOptionalProps = {
+	rootStyle: {
+		cursor: "pointer",
+		outline: "none",
+	},
+};
+
+const BarSmall = (props: IProps & typeof defaultProps) => {
 	// *** PROPS ***
 	const {
-		// arrowIndent,
-		isDateChangeable,
-		// isDelete,
-		isProgressChangeable,
-		isSelected,
-		onEventStart,
-		// rtl,
+		rootStyle,
 		task,
-		// taskHeight,
+		isSelected,
+		isDateChangeable,
+		isProgressChangeable,
+		onEventStart,
 	} = props;
 
 	// *** USE STATE ***
@@ -38,44 +54,50 @@ const BarSmall = (props: IProps) => {
 	);
 
 	return (
-		<g
-			className={styles.barWrapper}
-			tabIndex={0}
-			onMouseEnter={() => setIsHovered(() => true)}
-			onMouseLeave={() => setIsHovered(() => false)}
-		>
-			<BarDisplay
-				x={task.x1}
-				y={task.y}
-				width={task.x2 - task.x1}
-				height={task.height}
-				progressX={task.progressX}
-				progressWidth={task.progressWidth}
-				barCornerRadius={task.barCornerRadius}
-				fillStyle={task.styles}
-				isSelected={isSelected}
-				onMouseDown={(e) => {
-					isDateChangeable && onEventStart("move", task, e);
-				}}
-			/>
-			<g>
-				{isProgressChangeable && (
-					<BarProgressHandle
-						style={{
-							fill: "#ddd",
-							cursor: "ew-resize",
-							opacity: isHovered ? 1 : 0,
-							visibility: isHovered ? "visible" : "hidden",
-						}}
-						progressPoint={progressPoint}
-						onMouseDown={(e) => {
-							onEventStart("progress", task, e);
-						}}
-					/>
-				)}
+		<svg>
+			<g
+				style={rootStyle}
+				tabIndex={0}
+				onMouseEnter={() => setIsHovered(() => true)}
+				onMouseLeave={() => setIsHovered(() => false)}
+			>
+				{/* BAR */}
+				<BarDisplay
+					x={task.x1}
+					y={task.y}
+					width={task.x2 - task.x1}
+					height={task.height}
+					progressX={task.progressX}
+					progressWidth={task.progressWidth}
+					barCornerRadius={task.barCornerRadius}
+					fillStyle={task.styles}
+					isSelected={isSelected}
+					onMouseDown={(e) => {
+						isDateChangeable === true && onEventStart("move", task, e);
+					}}
+				/>
+
+				{/* PROGRESS HANDLE */}
+				<g>
+					{isProgressChangeable === true && (
+						<BarProgressHandle
+							style={{
+								fill: "#ddd",
+								cursor: "ew-resize",
+								opacity: isHovered ? 1 : 0,
+								visibility: isHovered ? "visible" : "hidden",
+							}}
+							progressPoint={progressPoint}
+							onMouseDown={(e) => {
+								onEventStart("progress", task, e);
+							}}
+						/>
+					)}
+				</g>
 			</g>
-		</g>
+		</svg>
 	);
 };
+BarSmall.defaultProps = defaultProps;
 
 export default BarSmall;
