558df4660326e7ecc62d40eae7c83cbe47803ce1
Nikita Smith <n.galkin.official@gmail.com>
2022-02-16 09:59:03 +0500

    refactor: 💡 GridBody refactor

--- src/lib/containers/Grid/components/GridBody.tsx
+++ src/lib/containers/Grid/components/GridBody.tsx
@@ -4,9 +4,7 @@ import React, { ReactChild } from "react";
 // *** OTHER ***
 import { Task } from "../../../types/public-types";
 import { addToDate } from "../../../helpers/date-helper";
-
-// *** STYLES ***
-import styles from "./GridBody.module.css";
+import { OptionalKeys } from "../../../types/custom";
 
 // *** TYPES ***
 export type IProps = {
@@ -17,78 +15,121 @@ export type IProps = {
 	columnWidth: number;
 	todayColor: string;
 	rtl: boolean;
+	// styles
+	gridRowStyle?: React.CSSProperties;
+	gridRowLineStyle?: React.CSSProperties;
+	gridTickStyle?: React.CSSProperties;
+};
+type TOptionalPropsKeys = Exclude<OptionalKeys<IProps>, undefined>;
+type TOptionalProps = Required<Pick<IProps, TOptionalPropsKeys>>;
+
+export const defaultProps: TOptionalProps = {
+	gridRowStyle: {
+		fill: "#fff",
+	},
+	gridRowLineStyle: {
+		stroke: "#ebeff2",
+	},
+	gridTickStyle: {
+		stroke: "#e6e4e4",
+	},
 };
 
-const GridBody = (props: IProps) => {
+const GridBody = (props: IProps & typeof defaultProps) => {
 	// *** PROPS ***
-	const { columnWidth, dates, rowHeight, rtl, svgWidth, tasks, todayColor } =
-		props;
+	const {
+		columnWidth,
+		dates,
+		rowHeight,
+		rtl,
+		svgWidth,
+		tasks,
+		todayColor,
+		// style
+		gridRowStyle,
+		gridRowLineStyle,
+		gridTickStyle,
+	} = props;
 
+	// *** CONDITIONALS ***
+	const nowDate = new Date();
 	let y = 0;
+	let tickX = 0;
+
+	// Grid Rows + Row Lines
 	const gridRows: ReactChild[] = [];
 	const rowLines: ReactChild[] = [
 		<line
 			key="RowLineFirst"
+			style={gridRowLineStyle}
 			x="0"
 			y1={0}
 			x2={svgWidth}
 			y2={0}
-			className={styles.gridRowLine}
 		/>,
 	];
+
 	for (const task of tasks) {
+		// Grid Rows
 		gridRows.push(
 			<rect
+				style={gridRowStyle}
 				key={"Row" + task.id}
 				x="0"
 				y={y}
 				width={svgWidth}
 				height={rowHeight}
-				className={styles.gridRow}
 			/>
 		);
+
+		// Rows Lines
 		rowLines.push(
 			<line
 				key={"RowLine" + task.id}
+				style={gridRowLineStyle}
 				x="0"
 				y1={y + rowHeight}
 				x2={svgWidth}
 				y2={y + rowHeight}
-				className={styles.gridRowLine}
 			/>
 		);
 		y += rowHeight;
 	}
 
-	const now = new Date();
-	let tickX = 0;
+	// Ticks + Today
 	const ticks: ReactChild[] = [];
 	let today: ReactChild = <rect />;
 	for (let i = 0; i < dates.length; i++) {
 		const date = dates[i];
+
+		// Ticks
 		ticks.push(
 			<line
 				key={date.getTime()}
+				style={gridTickStyle}
 				x1={tickX}
 				y1={0}
 				x2={tickX}
 				y2={y}
-				className={styles.gridTick}
 			/>
 		);
+
+		// Today
+		// DOES ANYONE CAN READ SUCH STUPID IF-STATEMENTS BRIEFLY????
+		// I DON'T THINK SO
 		if (
 			(i + 1 !== dates.length &&
-				date.getTime() < now.getTime() &&
-				dates[i + 1].getTime() >= now.getTime()) ||
+				date.getTime() < nowDate.getTime() &&
+				dates[i + 1].getTime() >= nowDate.getTime()) ||
 			// if current date is last
 			(i !== 0 &&
 				i + 1 === dates.length &&
-				date.getTime() < now.getTime() &&
+				date.getTime() < nowDate.getTime() &&
 				addToDate(
 					date,
 					date.getTime() - dates[i - 1].getTime(),
 					"millisecond"
-				).getTime() >= now.getTime())
+				).getTime() >= nowDate.getTime())
 		) {
 			today = (
 				<rect
@@ -100,12 +141,13 @@ const GridBody = (props: IProps) => {
 				/>
 			);
 		}
+
 		// rtl for today
 		if (
 			rtl &&
 			i + 1 !== dates.length &&
-			date.getTime() >= now.getTime() &&
-			dates[i + 1].getTime() < now.getTime()
+			date.getTime() >= nowDate.getTime() &&
+			dates[i + 1].getTime() < nowDate.getTime()
 		) {
 			today = (
 				<rect
@@ -117,16 +159,29 @@ const GridBody = (props: IProps) => {
 				/>
 			);
 		}
+
 		tickX += columnWidth;
 	}
+
 	return (
-		<g className="gridBody">
-			<g className="rows">{gridRows}</g>
-			<g className="rowLines">{rowLines}</g>
-			<g className="ticks">{ticks}</g>
-			<g className="today">{today}</g>
-		</g>
+		<svg>
+			{/* ROOT */}
+			<g>
+				{/* ROWS */}
+				<g>{gridRows}</g>
+
+				{/* ROW LINES */}
+				<g>{rowLines}</g>
+
+				{/* TICKS */}
+				<g>{ticks}</g>
+
+				{/* TODAY */}
+				<g>{today}</g>
+			</g>
+		</svg>
 	);
 };
+GridBody.defaultProps = defaultProps;
 
 export default GridBody;
