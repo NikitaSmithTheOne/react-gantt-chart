0db91fd068e27398581f36ad7e822697476b974e
Nikita Smith <n.galkin.official@gmail.com>
2022-02-28 16:09:38 +0500

    docs: ✏️ storybook: HorizontalScroll stories improvement

--- src/lib/components/HorizontalScroll.stories.tsx
+++ src/lib/components/HorizontalScroll.stories.tsx
@@ -1,5 +1,6 @@
 // *** NPM ***
 import React from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
 
 // *** OTHER ***
 import HorizontalScroll, {
@@ -9,18 +10,26 @@ import HorizontalScroll, {
 export default {
 	title: "lib/components/HorizontalScroll",
 	component: HorizontalScroll,
+} as ComponentMeta<typeof HorizontalScroll>;
+
+const Template: ComponentStory<typeof HorizontalScroll> = (args) => {
+	return <HorizontalScroll {...args} />;
 };
 
-export const Original = () => {
-	return (
-		<HorizontalScroll
-			scroll={100}
-			onScroll={(...args) => console.log(args)}
-			// style
-			bodyStyle={{
-				...horizontalScrollDefaultProps.bodyStyle,
-				width: 2000,
-			}}
-		/>
-	);
+export const Original = Template.bind({});
+Original.args = {
+	scroll: 0,
+	onScroll: (e) => console.log(e),
+	// styles
+	rootStyle: horizontalScrollDefaultProps.rootStyle,
+	bodyStyle: {
+		...horizontalScrollDefaultProps.bodyStyle,
+		width: 3000,
+	},
+};
+
+export const CustomScroll = Template.bind({});
+CustomScroll.args = {
+	...Original.args,
+	scroll: 200,
 };
