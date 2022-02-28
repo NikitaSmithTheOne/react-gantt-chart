cdedb51d7334806b60bfb314b7854601fa969726
Nikita Smith <n.galkin.official@gmail.com>
2022-02-28 16:51:59 +0500

    docs: ✏️ storybook: VerticalScroll stories improvement

--- src/lib/components/VerticalScroll.stories.tsx
+++ src/lib/components/VerticalScroll.stories.tsx
@@ -1,5 +1,6 @@
 // *** NPM ***
 import React from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
 
 // *** OTHER ***
 import VerticalScroll, {
@@ -9,22 +10,29 @@ import VerticalScroll, {
 export default {
 	title: "lib/components/VerticalScroll",
 	component: VerticalScroll,
+} as ComponentMeta<typeof VerticalScroll>;
+
+const Template: ComponentStory<typeof VerticalScroll> = (args) => {
+	return <VerticalScroll {...args} />;
 };
 
-export const Original = () => {
-	return (
-		<VerticalScroll
-			scroll={500}
-			onScroll={(...args) => console.log(args)}
-			// style
-			rootStyle={{
-				...verticalScrollDefaultProps.rootStyle,
-				height: 100,
-			}}
-			bodyStyle={{
-				...verticalScrollDefaultProps.bodyStyle,
-				height: 1000,
-			}}
-		/>
-	);
+export const Original = Template.bind({});
+Original.args = {
+	scroll: 0,
+	onScroll: (e) => console.log(e),
+	// style
+	rootStyle: {
+		...verticalScrollDefaultProps.rootStyle,
+		height: 200,
+	},
+	bodyStyle: {
+		...verticalScrollDefaultProps.bodyStyle,
+		height: 1000,
+	},
+};
+
+export const CustomScroll = Template.bind({});
+CustomScroll.args = {
+	...Original.args,
+	scroll: 200,
 };
