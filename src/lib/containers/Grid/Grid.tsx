d498277e23eaf20109c09bc50314d5c06eafee96
Nikita Smith <n.galkin.official@gmail.com>
2022-02-28 13:29:53 +0500

    refactor: 💡 Grid refactor
    GridBody component is removed

--- src/lib/containers/Grid/Grid.tsx
+++ src/lib/containers/Grid/Grid.tsx
@@ -1,23 +1,187 @@
 // *** NPM ***
-import React from "react";
+import React, { ReactChild } from "react";
 
 // *** OTHER ***
-import GridBody, { IProps as GridBodyProps } from "./components/GridBody";
+import { Task } from "../../types/public-types";
+import { addToDate } from "../../helpers/date-helper";
+import { OptionalKeys } from "../../types/custom";
 
 // *** TYPES ***
-export type IProps = GridBodyProps;
+export type IProps = {
+	tasks: Task[];
+	dates: Date[];
+	svgWidth: number;
+	rowHeight: number;
+	columnWidth: number;
+	todayColor: string;
+	rtl: boolean;
+	// styles
+	gridRowStyle?: React.CSSProperties;
+	gridRowLineStyle?: React.CSSProperties;
+	gridTickStyle?: React.CSSProperties;
+};
+type TOptionalPropsKeys = Exclude<OptionalKeys<IProps>, undefined>;
+type TOptionalProps = Required<Pick<IProps, TOptionalPropsKeys>>;
 
-// WHICH KIND OF JOKE IS THIS? =)
-const Grid = (props: IProps) => {
+export const defaultProps: TOptionalProps = {
+	gridRowStyle: {
+		fill: "#fff",
+	},
+	gridRowLineStyle: {
+		stroke: "#ebeff2",
+	},
+	gridTickStyle: {
+		stroke: "#e6e4e4",
+	},
+};
+
+const Grid = (props: IProps & typeof defaultProps) => {
+	// *** PROPS ***
+	const {
+		tasks,
+		dates,
+		rtl,
+		rowHeight,
+		columnWidth,
+		svgWidth,
+		todayColor,
+		// style
+		gridRowStyle,
+		gridRowLineStyle,
+		gridTickStyle,
+	} = props;
+
+	// *** CONDITIONALS ***
+	const nowDate = new Date();
+	let y = 0;
+	let tickX = 0;
+
+	// Grid Rows + Row Lines
+	const gridRows: ReactChild[] = [];
+	const rowLines: ReactChild[] = [
+		<line
+			key="RowLineFirst"
+			style={gridRowLineStyle}
+			x="0"
+			y1={0}
+			x2={svgWidth}
+			y2={0}
+		/>,
+	];
+
+	for (const task of tasks) {
+		// Grid Rows
+		gridRows.push(
+			<rect
+				style={gridRowStyle}
+				key={"Row" + task.id}
+				x="0"
+				y={y}
+				width={svgWidth}
+				height={rowHeight}
+			/>
+		);
+
+		// Rows Lines
+		rowLines.push(
+			<line
+				key={"RowLine" + task.id}
+				style={gridRowLineStyle}
+				x="0"
+				y1={y + rowHeight}
+				x2={svgWidth}
+				y2={y + rowHeight}
+			/>
+		);
+		y += rowHeight;
+	}
+
+	// Ticks + Today
+	const ticks: ReactChild[] = [];
+	let today: ReactChild = <rect />;
+	for (let i = 0; i < dates.length; i++) {
+		const date = dates[i];
+
+		// Ticks
+		ticks.push(
+			<line
+				key={date.getTime()}
+				style={gridTickStyle}
+				x1={tickX}
+				y1={0}
+				x2={tickX}
+				y2={y}
+			/>
+		);
+
+		// Today
+		// DOES ANYONE CAN READ SUCH STUPID IF-STATEMENTS BRIEFLY????
+		// I DON'T THINK SO
+		if (
+			(i + 1 !== dates.length &&
+				date.getTime() < nowDate.getTime() &&
+				dates[i + 1].getTime() >= nowDate.getTime()) ||
+			// if current date is last
+			(i !== 0 &&
+				i + 1 === dates.length &&
+				date.getTime() < nowDate.getTime() &&
+				addToDate(
+					date,
+					date.getTime() - dates[i - 1].getTime(),
+					"millisecond"
+				).getTime() >= nowDate.getTime())
+		) {
+			today = (
+				<rect
+					x={tickX}
+					y={0}
+					width={columnWidth}
+					height={y}
+					fill={todayColor}
+				/>
+			);
+		}
+
+		// rtl for today
+		if (
+			rtl &&
+			i + 1 !== dates.length &&
+			date.getTime() >= nowDate.getTime() &&
+			dates[i + 1].getTime() < nowDate.getTime()
+		) {
+			today = (
+				<rect
+					x={tickX + columnWidth}
+					y={0}
+					width={columnWidth}
+					height={y}
+					fill={todayColor}
+				/>
+			);
+		}
+
+		tickX += columnWidth;
+	}
+
 	return (
 		<svg style={{ overflow: "visible" }}>
 			{/* ROOT */}
 			<g>
-				{/* GRID BODY */}
-				<GridBody {...props} />
+				{/* ROWS */}
+				<g>{gridRows}</g>
+
+				{/* ROW LINES */}
+				<g>{rowLines}</g>
+
+				{/* TICKS */}
+				<g>{ticks}</g>
+
+				{/* TODAY */}
+				<g>{today}</g>
 			</g>
 		</svg>
 	);
 };
+Grid.defaultProps = defaultProps;
 
 export default Grid;
--- src/lib/containers/Grid/index.ts
+++ src/lib/containers/Grid/index.ts
@@ -2,10 +2,3 @@ export {
 	default as Grid,
 	type IProps as IGridProps,
 } from "./Grid";
-
-// components
-export {
-	default as GridBody,
-    defaultProps as gridBodyDefaultProps,
-	type IProps as IGridBodyProps,
-} from "./components/GridBody";
