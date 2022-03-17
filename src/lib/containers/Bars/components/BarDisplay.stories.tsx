60e01b2acbd18a200114753ce2ed43d27277410d
Nikita Smith <n.galkin.official@gmail.com>
2022-03-17 16:17:19 +0500

    docs: ✏️ storybook: BarDisplay stories improvement

--- src/lib/containers/Bars/components/BarDisplay.stories.tsx
+++ src/lib/containers/Bars/components/BarDisplay.stories.tsx
@@ -1,5 +1,6 @@
 // *** NPM ***
 import React from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
 
 // *** OTHER ***
 import BarDisplay, {
@@ -9,28 +10,24 @@ import BarDisplay, {
 export default {
 	title: "lib/containers/Bars/components/BarDisplay",
 	component: BarDisplay,
-};
+} as ComponentMeta<typeof BarDisplay>;
 
-// Original
-export const Original = () => {
+const Template: ComponentStory<typeof BarDisplay> = (args) => {
 	return (
 		<svg style={{ overflow: "visible" }}>
-			<BarDisplay />
+			<BarDisplay {...args} />
 		</svg>
 	);
 };
-Original.storyName = "Original";
 
-// Custom Width
-export const OriginalCustomWidth = () => {
-	return (
-		<svg style={{ overflow: "visible" }}>
-			<BarDisplay
-				// style
-				barStyle={{ ...barDisplayDefaultProps.barStyle, width: "700px" }}
-			/>
-			;
-		</svg>
-	);
+export const Original = Template.bind({});
+Original.args = barDisplayDefaultProps;
+
+export const CustomWidth = Template.bind({});
+CustomWidth.args = {
+	...Original.args,
+	barStyle: {
+		...Original.args.barStyle,
+		width: "700px",
+	},
 };
-OriginalCustomWidth.storyName = "Original Custom Width";
