8c378470021138056876a1492da5ffef0d0dd1d9
Nikita Smith <n.galkin.official@gmail.com>
2022-03-22 10:53:01 +0500

    docs: ✏️ storybook: TaskListHeader stories improvement

--- src/lib/containers/TaskList/components/TaskListHeader.stories.tsx
+++ src/lib/containers/TaskList/components/TaskListHeader.stories.tsx
@@ -1,47 +1,48 @@
 // *** NPM ***
 import React from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
 
 // *** OTHER ***
 import TaskListHeader, {
 	defaultProps as taskListHeaderDefaultProps,
 } from "./TaskListHeader";
 
+// *** CONSTANTS ***
+const HEADER_HEIGHT = 50;
+const ROW_WIDTH = 200;
+
 export default {
 	title: "lib/containers/TaskList/components/TaskListHeader",
 	component: TaskListHeader,
+} as ComponentMeta<typeof TaskListHeader>;
+
+const Template: ComponentStory<typeof TaskListHeader> = (args) => {
+	return <TaskListHeader {...args} />;
 };
 
-// Default
-export const Default = () => {
-	return <TaskListHeader />;
-};
+export const Original = Template.bind({});
+Original.args = { ...taskListHeaderDefaultProps };
 
-// Simple
-export const Simple = () => {
-	const headerHeight = 50;
-	const rowWidth = 200;
-
-	return (
-		<TaskListHeader
-			rootStyle={{
-				...taskListHeaderDefaultProps.rootStyle,
-				fontFamily: "sans-serif",
-				fontSize: "20px",
-			}}
-			headerStyle={{
-				...taskListHeaderDefaultProps.headerStyle,
-				height: headerHeight - 2,
-			}}
-			columnStyle={{
-				...taskListHeaderDefaultProps.columnStyle,
-				minWidth: rowWidth,
-				textAlign: "center",
-			}}
-			columnSeparatorStyle={{
-				...taskListHeaderDefaultProps.columnSeparatorStyle,
-				height: headerHeight * 0.5,
-				marginTop: headerHeight * 0.2,
-			}}
-		/>
-	);
+export const Custom = Template.bind({});
+Custom.args = {
+	...Original.args,
+	rootStyle: {
+		...taskListHeaderDefaultProps.rootStyle,
+		fontFamily: "sans-serif",
+		fontSize: "20px",
+	},
+	headerStyle: {
+		...taskListHeaderDefaultProps.headerStyle,
+		height: HEADER_HEIGHT - 2,
+	},
+	columnStyle: {
+		...taskListHeaderDefaultProps.columnStyle,
+		minWidth: ROW_WIDTH,
+		textAlign: "center",
+	},
+	columnSeparatorStyle: {
+		...taskListHeaderDefaultProps.columnSeparatorStyle,
+		height: HEADER_HEIGHT * 0.5,
+		marginTop: HEADER_HEIGHT * 0.2,
+	},
 };
